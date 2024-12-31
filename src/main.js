6f23134a45a5a1b5da610f6da874da6e78e9eec1
the-pro7 <ameyawemmanuel900@gmail.com>
2024-12-31 18:57:42 +0000

    Update main.js
    Fixed mobile bugs

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { setupCountdown } from "./countdown.js";
 document.querySelector("#app").innerHTML = `
  <main class="pt-20 flex items-center flex-col relative min-h-screen">
    <h1 class="text-6xl font-black text-center" data-header>It's almost <span class="bg-gradient-to-tr from-purple-500 to-orange-500 bg-clip-text text-transparent drop-shadow-2xl">2025!</span></h1>
-   <div class="mt-32">
+   <div class="mt-10 md:mt-32">
     <!-- Add the countdown component here -->
     <h1 class="text-2xl font-black tracking-wide text-center">Countdown to 2025!!</h1>
     <div id="counter" class="flex flex-col items-center justify-center gap-5 md:flex-row">
@@ -27,7 +27,7 @@ document.querySelector("#app").innerHTML = `
       </div>
     </div>
    </div>
-   <p class="absolute bottom-0 right-0 m-5">Built by <span class="font-bold bg-gradient-to-tr from-purple-500 to-orange-500 bg-clip-text text-transparent">Emmanuel Opoku-Ameyaw</span> for y'all with 💗</p>
+   <p class="m-10 absolute bottom-0 right-0 lg:m-5">Built by <span class="font-bold bg-gradient-to-tr from-purple-500 to-orange-500 bg-clip-text text-transparent">Emmanuel Opoku-Ameyaw</span> for y'all with 💗</p>
  </main>
 `;
 
